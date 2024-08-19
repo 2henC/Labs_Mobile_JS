636d234fc5c0882ede2e4da31721128ea1203959
2henC <nguyenthanh8a2@gmail.com>
2024-08-19 19:42:35 +0700

    complete JavaScript Fundamentals – Part 2 - Code Challenge #4

--- Labs/Lab1.js
+++ Labs/Lab1.js
@@ -161,16 +161,16 @@
 // Apply this to the team's average scores �
 
 // Code challenge #1
-D_array = [44, 23, 71];
-K_array = [65, 54, 49];
-calcAverage = (array) => {
-    sum = 0;
-    avg = 0;
-    for (let i = 0; i < array.length; i++) {
-        sum += array[i];
-    }
-    return avg = sum / array.length;
-};
+// D_array = [44, 23, 71];
+// K_array = [65, 54, 49];
+// calcAverage = (array) => {
+//     sum = 0;
+//     avg = 0;
+//     for (let i = 0; i < array.length; i++) {
+//         sum += array[i];
+//     }
+//     return avg = sum / array.length;
+// };
 
 // sử dụng reduce
 // calcAverage = (array) => {
@@ -250,29 +250,89 @@ calcAverage = (array) => {
 // Test data: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m
 // tall.
 
-const Mark = {
-    fullName: 'Mark Miller',
-    mass: 78,
-    height: 1.69,
-    calcBMI: function () {
-        this.BMI = this.mass / (this.height ** 2);
-        return this.BMI;
+// const Mark = {
+//     fullName: 'Mark Miller',
+//     mass: 78,
+//     height: 1.69,
+//     calcBMI: function () {
+//         this.BMI = this.mass / (this.height ** 2);
+//         return this.BMI;
+//     }
+// }
+
+// const John = {
+//     fullName: 'John Smith',
+//     mass: 92,
+//     height: 1.95,
+//     calcBMI: function () {
+//         this.BMI = this.mass / (this.height ** 2);
+//         return this.BMI;
+//     }
+// }
+
+// if (Mark.calcBMI() > John.calcBMI()) {
+//     console.log(`Chỉ số BMI của ${Mark.fullName} (${Mark.calcBMI().toFixed(2)}) cao hơn so với của ${John.fullName} (${John.calcBMI().toFixed(2)})!`);
+// }
+// else {
+//     console.log(`Chỉ số BMI của ${Mark.fullName} (${Mark.calcBMI().toFixed(2)}) nhỏ hơn so với của ${John.fullName} (${John.calcBMI().toFixed(2)})!`);
+// }
+
+// Coding Challenge #4
+// Let's improve Steven's tip calculator even more, this time using loops!
+// Your tasks:
+// 1. Create an array 'bills' containing all 10 test bill values
+// 2. Create empty arrays for the tips and the totals('tips' and 'totals')
+// 3. Use the 'calcTip' function we wrote before(no need to repeat) to calculate
+// tips and total values(bill + tip) for every bill value in the bills array.Use a for
+// loop to perform the 10 calculations!
+// Test data: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52
+// Hints: Call ‘calcTip ‘in the loop and use the push method to add values to the
+// tips and totals arrays 😉
+// Bonus:
+// 4. Bonus: Write a function 'calcAverage' which takes an array called 'arr' as
+//     an argument.This function calculates the average of all numbers in the given
+// array.This is a difficult challenge(we haven't done this before)! Here is how to
+// solve it:
+//     4.1.First, you will need to add up all values in the array.To do the addition,
+//         start by creating a variable 'sum' that starts at 0. Then loop over the
+// array using a for loop.In each iteration, add the current value to the
+// 'sum' variable.This way, by the end of the loop, you have all values
+// added together
+// 4.2.To calculate the average, divide the sum you calculated before by the
+// length of the array(because that's the number of elements)
+// 4.3.Call the function with the 'totals' array
+
+bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+tips = [];
+totals = [];
+
+calcTip = (bill) => {
+    if (bill >= 50 && bill <= 300) {
+        return bill * 0.15;
+    }
+    else {
+        return bill * 0.2;
     }
 }
 
-const John = {
-    fullName: 'John Smith',
-    mass: 92,
-    height: 1.95,
-    calcBMI: function () {
-        this.BMI = this.mass / (this.height ** 2);
-        return this.BMI;
-    }
+bills.reduce((acc, cur) => {
+    tips.push(calcTip(cur));
+    totals.push(cur + calcTip(cur));
+}, 0);
+
+// Hoặc dùng for
+// for (let i = 0; i < bills.length; i++) {
+//     tips.push(calcTip(bills[i]));
+//     totals.push(bills[i] + calcTip(bills[i]));
+// }
+
+console.log(`Danh sách tiền tip: ${tips}`);
+console.log(`Danh sách tổng tiền: ${totals}`);
+
+// Sử dụng reduce
+calcAverage = (arr) => {
+    return arr.reduce((acc, cur) => (cur + acc), 0) / arr.length;
 }
 
-if (Mark.calcBMI() > John.calcBMI()) {
-    console.log(`Chỉ số BMI của ${Mark.fullName} (${Mark.calcBMI().toFixed(2)}) cao hơn so với của ${John.fullName} (${John.calcBMI().toFixed(2)})!`);
-}
-else {
-    console.log(`Chỉ số BMI của ${Mark.fullName} (${Mark.calcBMI().toFixed(2)}) nhỏ hơn so với của ${John.fullName} (${John.calcBMI().toFixed(2)})!`);
-}
+console.log(`Trung bình tổng tiền : ${calcAverage(totals)}`);
+
