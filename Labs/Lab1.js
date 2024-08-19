6be94a356aeb43c6324e2dfe5a2416f6c7364aa2
2henC <nguyenthanh8a2@gmail.com>
2024-08-19 17:01:57 +0700

    complete Part2 challenge #2

--- Labs/Lab1.js
+++ Labs/Lab1.js
@@ -178,21 +178,59 @@ calcAverage = (array) => {
 //     return sum / array.length;
 // };
 
-console.log(calcAverage(D_array));
-console.log(calcAverage(K_array));
+// console.log(calcAverage(D_array));
+// console.log(calcAverage(K_array));
 
-checkWinner = (a, b) => {
-    if (a >= 2 * b) {
-        console.log(`Dolphins win (${a} vs. ${b})`);
-    }
-    else if (b >= 2 * a) {
-        console.log(`Koalas win (${b} vs. ${a})`);
+// checkWinner = (a, b) => {
+//     if (a >= 2 * b) {
+//         console.log(`Dolphins win (${a} vs. ${b})`);
+//     }
+//     else if (b >= 2 * a) {
+//         console.log(`Koalas win (${b} vs. ${a})`);
+//     }
+//     else {
+//         console.log("No team wins!");
+//     }
+// }
+
+// checkWinner(calcAverage(D_array), calcAverage(K_array));
+
+
+// Coding Challenge #2
+// Steven is still building his tip calculator, using the same rules as before: Tip 15% of
+// the bill if the bill value is between 50 and 300, and if the value is different, the tip is
+// 20%.
+// Your tasks:
+// 1. Write a function 'calcTip' that takes any bill value as an input and returns
+// the corresponding tip, calculated based on the rules above (you can check out
+// the code from first tip calculator challenge if you need to). Use the function
+// type you like the most. Test the function using a bill value of 100
+// 2. And now let's use arrays! So create an array 'bills' containing the test data
+// below
+// 3. Create an array 'tips' containing the tip value for each bill, calculated from
+// the function you created before
+// 4. Bonus: Create an array 'total' containing the total values, so the bill + tip
+// Test data: 125, 555 and 44
+// Hint: Remember that an array needs a value in each position, and that value can
+// actually be the returned value of a function! So you can just call a function as array
+// values (so don't store the tip values in separate variables first, but right in the new
+// array) �
+
+bills = [125, 555, 44];
+calcTip = (bill) => {
+    if (bill >= 50 && bill <= 300) {
+        return bill * 0.15;
     }
     else {
-        console.log("No team wins!");
+        return bill * 0.2;
     }
 }
 
-checkWinner(calcAverage(D_array), calcAverage(K_array));
+console.log(`Tiền tip khi gia tri bill la 100: ${calcTip(100)}`);
 
+tips = bills.map(bill => calcTip(bill));
+console.log(`Danh sách tiền tip: ${tips}`);
 
+total = bills.map((bill) => bill + calcTip(bill));
+// total = bills.map((bill, index) => bill + tips[index]);
+console.log(`Danh sách tổng tiền: ${total}`);
